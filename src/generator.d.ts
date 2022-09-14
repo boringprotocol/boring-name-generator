d3355501e44d5ea0399061081e079f455a87be2c
Fran Swan <fran@boringprotocol.io>
2022-09-14 02:41:09 -0700

    initial commit

---
+++ src/generator.d.ts
@@ -0,0 +1,15 @@
+declare module 'boring-name-generator' {
+  interface Options {
+    alliterative?: boolean;
+    number?: boolean;
+    words?: number;
+  }
+
+  type Result = {
+    dashed: string;
+    raw: string[];
+    spaced: string;
+  };
+
+  export default function(options?: Options): Result;
+}
